3187a4d7ea179a6d3592359e151c9f419dcf3822
EklektikArtist <infoforyou42@gmail.com>
2022-03-03 21:01:27 -0600

    More MPI output

--- code/main.cpp
+++ code/main.cpp
@@ -1272,9 +1272,25 @@ void update_species
 
 void mpi_test()
 {
-    MPI_Init( NULL, NULL);
-    int my_rank;
-    MPI_Comm_rank( MPI_COMM_WORLD, &my_rank);
-    printf( "Echo from rank %d\n", my_rank );
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    printf("Hello world from processor %s, rank %d out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    // Finalize the MPI environment.
     MPI_Finalize();
 }
